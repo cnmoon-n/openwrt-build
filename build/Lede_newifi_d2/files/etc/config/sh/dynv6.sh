68238086d388f1c4d326f52abcd24ae59cc8515c
cncot <95631281+cncot@users.noreply.github.com>
2022-11-19 18:55:18 +0800

    1

--- build/Lede_newifi_d2/files/etc/config/sh/dynv6.sh
+++ build/Lede_newifi_d2/files/etc/config/sh/dynv6.sh
@@ -4,5 +4,6 @@
 ###unas 的dev 是ens18 openwrt 是pppoe-wan/br-lan
 ip=`ip -6 addr show dev br-lan |grep 'scope global'|grep -v deprecated|awk -F '/|inet6 ' 'NR==1{print $2;}' 2>&1` || die "$ip"
 curl "https://dynv6.com/api/update?hostname=etceye.dynv6.net&ipv6=$ip&token=EEKykqBBQ7De2ky1xKN3BBEQNuoBrn" -k > /etc/config/sh/dynv6.log
+curl "https://dynv6.com/api/update?hostname=eye10001.dns.army&ipv6=$ip&token=876xxUQDk5yvkK4WL74izEeSbiCn7k" -k >> /etc/config/sh/dynv6.log
 
 curl -X POST https://dnsapi.cn/Record.Modify -d "login_token=337504,45c889017749b3d0d608f3224ce6f12a&record_id=1183678299&format=json&domain=etceye.eu.org&sub_domain=eye100hax&value=$ip&record_type=AAAA&record_line=%e9%bb%98%e8%ae%a4" >> /etc/config/sh/dynv6.log
--- build/Lede_newifi_y1s/files/etc/config/sh/dynv6.sh
+++ build/Lede_newifi_y1s/files/etc/config/sh/dynv6.sh
@@ -1,8 +1,11 @@
-#curl -6 "https://dynv6.com/api/update?hostname=etceye.dynv6.net&ipv6=auto&token=EEKykqBBQ7De2ky1xKN3BBEQNuoBrn" > /etc/config/sh/dynv6.log
-
 #!/bin/sh
 ###unas 的dev 是ens18 openwrt 是pppoe-wan/br-lan
-ip=`ip -6 addr show dev br-lan |grep 'scope global'|grep -v deprecated|awk -F '/|inet6 ' 'NR==1{print $2;}' 2>&1` || die "$ip"
-curl "https://dynv6.com/api/update?hostname=etceye.dynv6.net&ipv6=$ip&token=EEKykqBBQ7De2ky1xKN3BBEQNuoBrn" -k > /etc/config/sh/dynv6.log
+ip=`ip -6 addr show dev eth0.2 |grep 'scope global'|grep -v deprecated|awk -F '/|inet6 ' 'NR==1{print $2;}' 2>&1` || die "$ip"
+curl "http://dynv6.com/api/update?hostname=eye10001.dynv6.net&ipv6=$ip&token=876xxUQDk5yvkK4WL74izEeSbiCn7k" -k > /etc/config/sh/curl.log
 
-curl -X POST https://dnsapi.cn/Record.Modify -d "login_token=337504,45c889017749b3d0d608f3224ce6f12a&record_id=1183678299&format=json&domain=etceye.eu.org&sub_domain=eye100hax&value=$ip&record_type=AAAA&record_line=%e9%bb%98%e8%ae%a4" >> /etc/config/sh/dynv6.log
+###curl "http://dynv6.com/api/update?hostname=eye10000.dynv6.net&token=876xxUQDk5yvkK4WL74izEeSbiCn7k&ipv6=$ip" -k
+
+curl -X POST https://dnsapi.cn/Record.Modify -d "login_token=337504,45c889017749b3d0d608f3224ce6f12a&record_id=1183906781&format=json&domain=etceye.eu.org&sub_domain=eye10gz&value=$ip&record_type=AAAA&record_line=%e9%bb%98%e8%ae%a4" >> /etc/config/sh/curl.log
+
+
+ 
